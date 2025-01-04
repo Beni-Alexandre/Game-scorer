6beeca9996879590a3fc08bd5ce5a6f353aeb3dc
Beni-Alexandre <honfogabeni@gmail.com>
2025-01-04 22:19:44 +0100

    end

---
+++ basketball.js
@@ -0,0 +1,37 @@
+//team1
+var score1 = 0;
+ var scoreDisplay1 = document.getElementById("scoreDisplay1");
+
+ function increaseScore1(points){
+    score1 += points;
+    updateScoreDisplay1();
+ }
+ function resetScore1(points){
+    score1 = 0;
+    updateScoreDisplay1();
+ }
+ function updateScoreDisplay1() {
+    scoreDisplay1.textContent = score1;
+  }
+  //team2
+   var score2 = 0;
+     var scoreDisplay2 = document.getElementById("scoreDisplay2");
+
+     function increaseScore2(points){
+        score2 += points;
+        updateScoreDisplay2();
+     }
+     function resetScore2(points){
+        score2 = 0;
+        updateScoreDisplay2();
+     }
+     function updateScoreDisplay2(){
+        scoreDisplay2.textContent = score2;
+     }
+     var Menu = document.getElementById("Menu");
+     function hideMenu(){
+       Menu.style.fontSize = "0px";
+     }
+     function showMenu(){
+       Menu.style.fontSize = "12px";
+     }
